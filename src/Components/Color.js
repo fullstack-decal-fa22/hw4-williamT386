eb1ced0c4885babb20a448d92bbe0ace2a79f61f
William Tang <williamtang.basis@gmail.com>
2022-10-21 12:04:58 -0700

    Q2

--- src/Components/Color.js
+++ src/Components/Color.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
 /* TODO: Translate the below class component to a functional component! */
-class Color extends React.Component {
+const Color = () => {
 
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return <button onClick={() => this.props.handleClick(this.props.color)}>Post {this.props.color}</button>
-    }
+    return (
+        <button onClick={() => this.props.handleClick(this.props.color)}>Post {this.props.color}</button>
+    );
 }
 
 export default Color;
