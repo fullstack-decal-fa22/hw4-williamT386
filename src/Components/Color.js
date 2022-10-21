d40650e2cc9de8d730665c67e746807e78ffe9c0
William Tang <williamtang.basis@gmail.com>
2022-10-21 12:31:27 -0700

    Added props q2

--- src/Components/Color.js
+++ src/Components/Color.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 /* TODO: Translate the below class component to a functional component! */
-const Color = () => {
+const Color = (props) => {
 
     return (
         <button onClick={() => this.props.handleClick(this.props.color)}>Post {this.props.color}</button>
