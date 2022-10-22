6bb196507b7d74940bc20aac7f7279485ef455ba
William Tang <williamtang.basis@gmail.com>
2022-10-22 09:29:08 -0700

    Try to create G Pages

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,12 +1,23 @@
 import React from 'react';
-/* Add any imports you think you might need here! */
+import Color from './Color';
+import { useState } from 'react';
 
-const Menu = () => { 
+const Menu = () => {
+    const [old_color, setColor] = useState("");
+
+    const useColor = (new_color) => {
+        setColor(new_color);
+        // return <Color color={old_color}></Color>
+        // return <button></button>
+    }
 
     return (
-      <div className="colorOptions">
-          {/* TODO */}
-      </div>
+        <div className="colorOptions">
+            <button>Post blue</button>
+            <button onClick={() => { setColor('red'); }}>Post blue</button>
+            <p>{old_color + "3"}</p>
+            <Color handleClick={() => console.log('hi')} color={old_color}></Color>
+        </div >
     );
 }
 
